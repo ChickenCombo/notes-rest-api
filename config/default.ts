5102275ef021b9a600ab05c15d6d4dccb7b426ff
Lester Cuadra <lester.cuadra24@gmail.com>
2023-05-19 12:58:51 +0800

    feat: user model

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
   port: 5000,
   dbUri: "mongodb://127.0.0.1:27017/notes-rest-api",
+  saltWorkFactor: 10,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import config from "config";
 import connect from "./utils/connect";
-import logger from "./utils/logger"
+import logger from "./utils/logger";
+import routes from "./routes";
 
 const port = config.get<number>("port");
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello World!");
-});
-
 app.listen(port, async () => {
   logger.info(`Server is up and running at http://localhost:${port}`);
+
   await connect();
+
+  routes(app);
 });
